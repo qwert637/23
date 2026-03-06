b2d9878d54a234dbb929d3fda697b6d3983efdfc
qwert637 <yuragrubanego@gmail.com>
2026-03-06 07:59:34 +0000

    ghh

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+int A, B;
+cout <<"Введіть значення А: ";
+cin >> A;
+cout <<"Введіть значення В: ";
+cin >> B; 
+cout <<"A && B: " << (A && B) << endl; 
+cout <<"A || B: " << (A || B) << endl;
+cout <<"A ^ B: " << (A ^ B) << endl;
+return 0;
+}
